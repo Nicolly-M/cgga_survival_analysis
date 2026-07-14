6a41bf9162761fb61b98ae9ef2af47ab3f197323
Nicolly Clemente de Melo <nicolly.melo@mail.usf.edu.br>
2026-07-14 12:46:07 -0300

    Add files via upload

---
+++ cgga_survival_analysis_code.R
@@ -0,0 +1,121 @@
+#install.packages(c("dplyr","tidyr","survival","survminer","ggplot2","gridExtra"))
+
+library(dplyr)
+library(tidyr)
+library(survival)
+library(survminer)
+library(ggplot2)
+library(gridExtra)
+
+
+clin <- clinical.file
+clin$OS     <- as.numeric(clin$OS)
+clin$Censor <- as.numeric(clin$Censor)
+
+expr <- expr.file
+gene.col <- names(expr)[1]
+
+
+
+#### EGFR primary ####
+
+
+egfr.dat <- get_gene_data("EGFR")
+egfr.primary <- filter_group(egfr.dat, "Primary")
+
+med.exp <- median(egfr.primary$Expression)
+
+n.high <- sum(egfr.primary$Expression >= med.exp)
+n.low  <- sum(egfr.primary$Expression < med.exp)
+
+egfr.primary$status <- ifelse(
+  egfr.primary$Expression >= med.exp,
+  paste0("High (", n.high, ")"),
+  paste0("Low (", n.low, ")")
+)
+
+high_lab <- paste0("High (", n.high, ")")
+low_lab  <- paste0("Low (", n.low, ")")
+
+egfr.primary$status <- factor(
+  egfr.primary$status,
+  levels = c(high_lab, low_lab)
+)
+
+fit <- survfit(
+  Surv(OS, Censor) ~ status,
+  data = egfr.primary
+)
+
+# Graph
+egfr.plot.primary <- ggsurvplot(
+  fit,
+  data = egfr.primary,
+  pval = TRUE,
+  xlab = "Time (days)",
+  pval.coord = c(3500, 0.98),
+  ggtheme = theme_light() +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    ),  
+  surv.median.line = "hv",
+  title = "EGFR (WHO IV Primary)",
+  palette = c("#FF5C00", "#6900A8"),
+  legend.title = "Expression",
+  legend.labs = c(high_lab, low_lab)
+)
+
+#print(egfr.plot.primary)
+
+
+#### EGFR Recurrent ####
+
+egfr.recurrent <- filter_group(egfr.dat, "Recurrent")
+
+med.exp <- median(egfr.recurrent$Expression)
+
+n.high <- sum(egfr.recurrent$Expression >= med.exp)
+n.low  <- sum(egfr.recurrent$Expression < med.exp)
+
+egfr.recurrent$status <- ifelse(
+  egfr.recurrent$Expression >= med.exp,
+  paste0("High (", n.high, ")"),
+  paste0("Low (", n.low, ")")
+)
+
+high_lab <- paste0("High (", n.high, ")")
+low_lab  <- paste0("Low (", n.low, ")")
+
+egfr.recurrent$status <- factor(
+  egfr.recurrent$status,
+  levels = c(high_lab, low_lab)
+)
+
+fit <- survfit(
+  Surv(OS, Censor) ~ status,
+  data = egfr.recurrent
+)
+
+# Graph 
+egfr.plot.recurrent <- ggsurvplot(
+  fit,
+  data = egfr.recurrent,
+  pval = TRUE,
+  xlab = "Time (days)",
+  pval.coord = c(450, 0.98),
+  ggtheme = theme_light() +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    ), 
+  surv.median.line = "hv",
+  title = "EGFR (WHO IV Recurrent)",
+  palette = c("#FF5C00", "#6900A8"),
+  legend.title = "Expression",
+  legend.labs = c(high_lab, low_lab)
+)
+
+#print(egfr.plot.recurrent)
+
+
+#ggsave(filename = "mcl1.plot.recurrent.png", plot = mcl1.plot.recurrent$plot, width = 6, height = 4, dpi = 300)
+
